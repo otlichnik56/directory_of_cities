6b71df925d2d8fdbdd8e0d23eea094f409a8f367
Hyp <otlichnik56@yandex.ru>
2023-06-07 19:13:56 +0500

    task №4 подсчёт количества городов по областям

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,37 +8,8 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        BufferedReader reader = new BufferedReader(new FileReader(
-                "directory-of-cities.csv"));
-        String line = null;
-        Scanner scanner = null;
-        int index = 0;
-        List<City> cityList = new ArrayList<>();
-
-        while ((line = reader.readLine()) != null) {
-            City city = new City();
-            scanner = new Scanner(line);
-            scanner.useDelimiter(";");
-            while (scanner.hasNext()) {
-                String data = scanner.next();
-                if (index == 1)
-                    city.setName(data);
-                else if (index == 2)
-                    city.setRegion(data);
-                else if (index == 3)
-                    city.setDistrict(data);
-                else if (index == 4)
-                    city.setPopulation(Integer.parseInt(data));
-                else if (index == 5)
-                    city.setFoundation(data);
-                index++;
-            }
-            index = 0;
-            cityList.add(city);
-        }
-        reader.close();
-        maxPopulation(cityList);
+        String pathFile = "directory-of-cities.csv";
+        numberOfCities(readFileAdnCreateCityList(pathFile));
 
     }
 
@@ -70,5 +41,48 @@ public class Main {
         }
     }
 
+    private static void numberOfCities(List<City> cityList) {
+        if (cityList == null) {
+            System.out.println("directory not found");
+        } else {
+            Map<String, Integer> counts = new HashMap<>();
+            for (City city : cityList) {
+                counts.merge(city.getRegion(), 1, Integer::sum);
+            }
+            for (Map.Entry<String, Integer> entry : counts.entrySet()) {
+                System.out.println(entry.getKey() + " - " + entry.getValue());
+            }
+        }
+    }
+
+    private static List<City> readFileAdnCreateCityList(String pathFile) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(pathFile));
+        String line;
+        int index = 0;
+        List<City> cityList = new ArrayList<>();
+        while ((line = reader.readLine()) != null) {
+            City city = new City();
+            Scanner scanner = new Scanner(line);
+            scanner.useDelimiter(";");
+            while (scanner.hasNext()) {
+                String data = scanner.next();
+                if (index == 1)
+                    city.setName(data);
+                else if (index == 2)
+                    city.setRegion(data);
+                else if (index == 3)
+                    city.setDistrict(data);
+                else if (index == 4)
+                    city.setPopulation(Integer.parseInt(data));
+                else if (index == 5)
+                    city.setFoundation(data);
+                index++;
+            }
+            index = 0;
+            cityList.add(city);
+        }
+        reader.close();
+        return cityList;
+    }
 
 }
